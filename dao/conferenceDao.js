687de5af2ac3549473e1aa1aea17aa7089764bc1
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2014-02-19 18:29:06 +0530

    fixing daos

--- dao/conferenceDao.js
+++ dao/conferenceDao.js
@@ -1,36 +1,61 @@
-var log = new Log();
+/* jshint indent: false */
 
-var db = require('/dao/db.js').db;
+'use strict';
 
-var createConference = function (conference){
-    var json = JSON.parse(conference);
-    var result = db.query("INSERT INTO tbl_conference (name, location, startDate, endDate, logo, status) " +
-        "VALUES (?, ?, ?, ?, ?)",
-        json.name, json.location, json.startDate, json.endDate, json.logo, status);
-    db.commit();
+var db = require('./db.js').db;
+var utils = require('./utils.js');
+
+var createConference = function(conf) {
+    var values = [
+        conf.name,
+        conf.location,
+        conf.startDate,
+        conf.endDate,
+        conf.logo,
+        1
+    ];
+
+    utils.parseValues(values);
+
+    var query = 'INSERT INTO tbl_conference (name, location, startDate, endDate, logo, status) ' +
+        'VALUES (' + values.toString() + ')';
+
+    db.query(query);
+
+    // we may need to find a way to return the result, but db.query() does not
+    // do this
+    return;
+};
+
+var updateConference = function(conf, id) {
+
+    db.query('UPDATE tbl_conference SET ' +
+        'name = ' + utils.parseValue(conf.name) + ', ' +
+        'location = ' + utils.parseValue(conf.location) + ', ' +
+        'startDate = ' + utils.parseValue(conf.startDate) + ', ' +
+        'endDate = ' + utils.parseValue(conf.endDate) + ', ' +
+        'logo = ' + utils.parseValue(conf.logo) + ' ' +
+        'WHERE id = ' + utils.parseValue(id));
+
+    return;
+};
+
+var getConferenceByConferenceId = function(conferenceId) {
+    var result = db.query('SELECT id, name, location, ' +
+        'UNIX_TIMESTAMP(startDate)*1000 AS startDate, ' +
+        'UNIX_TIMESTAMP(endDate)*1000 AS endDate, ' +
+        'logo FROM tbl_conference WHERE id = ' +
+        utils.parseValue(conferenceId));
+
     return result;
-}
+};
 
-var updateConference = function (conference){
-    var json = JSON.parse(conference);
-    var result = db.query("UPDATE tbl_conference SET name = ?, location = ?, startDate = ?, endDate = ?, logo = ?, " +
-        "status = ? WHERE id = ?",
-        json.name, json.location, json.startDate, json.endDate, json.logo, json.status, json.id);
-    db.commit();
+var getAllConferences = function() {
+    var result = db.query('SELECT * FROM tbl_conference');
     return result;
-}
+};
 
-var getConferenceByConferenceId = function (conferenceId) {
-    var result = db.query("SELECT * FROM tbl_conference WHERE id = ?", conferenceId);
+var getAllActiveConferences = function() {
+    var result = db.query('SELECT * FROM tbl_conference WHERE status = 1');
     return result;
-}
-
-var getAllConferences = function () {
-    var result = db.query("SELECT * FROM tbl_conference");
-    return result;
-}
-
-var getAllActiveConferences = function () {
-    var result = db.query("SELECT * FROM tbl_conference WHERE status = 1");
-    return result;
-}
+};
---
+++ dao/utils.js
@@ -0,0 +1,24 @@
+/* jshint indent: false */
+
+'use strict';
+
+// takes an array of SQL values and parses them for safe CRUD
+var parseValues = function(arr) {
+	if (arr instanceof Array) {
+		arr.forEach(function(element, index, array) {
+			array[index] = parseValue(element);
+		});
+	} else {
+		var log = new Log();
+		log.warn('Non-array passed as SQL values');
+	}
+};
+
+// takes a value and parses it for safe CRUD
+var parseValue = function(value) {
+	if (typeof value === 'string') {
+		return '"' + value + '"';
+	} else {
+		return value;
+	}
+};
