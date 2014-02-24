041602fa402258ba88330119fc0812b52c1f8fbb
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2014-02-24 11:28:32 +0530

    conference booth fix

--- routes/conferenceBoothRouter.js
+++ routes/conferenceBoothRouter.js
@@ -7,30 +7,32 @@ var conferenceBoothDao = require('/dao/conferenceBoothDao.js');
 var postConferenceBooth = function (req, res) {
     var conferenceBooth = JSON.parse(req.getContent());
 
-    if (!conferenceBooth.conferenceId || !conferenceBooth.boothId || !conferenceBooth.status) {
+    if (!conferenceBooth.conferenceId || !conferenceBooth.boothId) {
         res.status = 400;
         res.contentType = 'application/json';
         res.content = {
             message: 'Conference Booth should contain conferenceId, boothId and status'
         };
     } else {
-        res.status = 200;
+        conferenceBooth.status = 1;
         conferenceBoothDao.createConferenceBooth(conferenceBooth);
+        res.status = 200;
     }
 }
 
 var putConferenceBooth = function (req, res) {
     var conferenceBooth = JSON.parse(req.getContent());
 
-    if (!conferenceBooth.conferenceId || !conferenceBooth.boothId || !conferenceBooth.status) {
+    if (!conferenceBooth.conferenceId || !conferenceBooth.boothId) {
         res.status = 400;
         res.contentType = 'application/json';
         res.content = {
             message: 'Conference Booth should contain conferenceId, boothId and status'
         };
     } else {
-        res.status = 200;
+        conferenceBooth.status = conferenceBooth.status || 1;
         conferenceBoothDao.updateConferenceBooth(conferenceBooth);
+        res.status = 200;
     }
 }
 
